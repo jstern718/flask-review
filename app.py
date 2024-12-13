9297651cbd3cb1121af16b853766130944c3a59d
Jonathan Stern <jstern718@gmail.com>
2024-12-12 21:45:58 -0500

    following warbler

--- app.py
+++ app.py
@@ -1,45 +1,136 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask
+
+from flask import Flask, flash, g, redirect, render_template, request, url_for
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
 
 load_dotenv()  # Load environment variables from .env file
-secret_key = os.getenv("SECRET_KEY")
+
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+
+app.config['secret_key']=os.environ('SECRET_KEY'),
+app.config['DATABASE']=os.path.join(app.instance_path, 'flaskr.sqlite'),
+)
+
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+from .flaskr import db
+db.init_app(app)
+
+from .flaskr import auth
+app.register_blueprint(auth.bp)
+
+from .flaskr import blog
+app.register_blueprint(blog.bp)
+app.add_url_rule('/', endpoint='index')
+
+    # a simple page that says hello
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+@app.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('index.html', posts=posts)
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY=secret_key,
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+@app.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+        if not title:
+            error = 'Title is required.'
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('index'))
 
-    from .flaskr import db
-    db.init_app(app)
+    return render_template('blog/create.html')
 
-    from .flaskr import auth
-    app.register_blueprint(auth.bp)
 
-    from .flaskr import blog
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
 
-     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
 
-    return app
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@app.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('index'))
